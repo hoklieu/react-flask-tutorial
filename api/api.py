f17a8b4c8093b8c0cde7bbf03afe6257d6e5ca59
hoklieu <song51689@gmail.com>
2020-05-08 22:26:06 -0400

    add useState and useEffect to get/set time which is fetched through API call to /api/time

--- api/api.py
+++ api/api.py
@@ -1,9 +1,13 @@
 from flask import Flask
 import time
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../build", static_url_path="/")
 
-@app.route('/time')
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/api/time')
 def get_current_time():
     # Flask automatically Jsonify dict for you
     return {'time': time.time()}
